b57d01eaa23e52dc769443e6877329fc0d7cfb22
sunnyshrestha <dev.suncha@gmail.com>
2015-01-18 23:07:18 +0545

    Added avtivity

--- app/src/main/java/dev/suncha/blooddonationrecord/MainActivity.java
+++ app/src/main/java/dev/suncha/blooddonationrecord/MainActivity.java
@@ -2,6 +2,7 @@ package dev.suncha.blooddonationrecord;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -17,6 +19,7 @@ public class MainActivity extends Activity {
     ListView lv;
     Context context;
     ImageView topimageview;
+    TextView proud_tv;
 
     String[] itemname ={
             "Add new entry",
@@ -45,11 +48,23 @@ public class MainActivity extends Activity {
         topimageview=(ImageView)findViewById(R.id.topimage);
         topimageview.getLayoutParams().height=height/4;
 
+        proud_tv=(TextView)findViewById(R.id.proud_text);
+        proud_tv.getLayoutParams().height=height/4;
+
+        final Intent donationDetails=new Intent(this,getDonationDetails.class);
+
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(context, "You Clicked " + itemname[position], Toast.LENGTH_SHORT).show();
+                //Toast.makeText(context, "You Clicked " + itemname[position], Toast.LENGTH_SHORT).show();
+                switch(position){
+                    case 0:
+                        startActivity(donationDetails);
+                        break;
+
+
+                }
             }
         });
     }
